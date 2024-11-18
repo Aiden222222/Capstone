d2dddc621966920e89dd68fbb3ca05bf698fee1d
Priyam Modi <modipriyam20@gmail.com>
2024-11-18 13:33:50 -0500

    debugged cooler, lid

--- main/main.c
+++ main/main.c
@@ -8,43 +8,51 @@
 #include "esp_log.h"
 #include "sdkconfig.h"
 
-static const char *TAG = "Capstone M3";
+static const char *TAG = "\n";
 static esp_adc_cal_characteristics_t adc1_chars;
 static ledc_channel_config_t cooler_pwm_channel;
+
+// Cooler PWM
+#define COOLER_TIMER              LEDC_TIMER_0
+#define COOLER_MODE               LEDC_LOW_SPEED_MODE
+#define COOLER_OUTPUT_IO          (4) // Define the output GPIO
+#define COOLER_CHANNEL            LEDC_CHANNEL_0
+#define COOLER_DUTY_RES           LEDC_TIMER_4_BIT // Set duty resolution to 13 bits
+#define COOLER_DUTY               (10) // Set duty to 70%. (2 ** 4) * 70% = 8
+#define COOLER_FREQUENCY          (200000) // Frequency in Hertz. Set frequency at 4 kHz
 
 /* Use project configuration menu (idf.py menuconfig) to choose the GPIO to blink,
    or you can edit the following line and set a number here.
 */
 
-static uint32_t analog_read(adc1_channel_t adc_channel){
-    uint32_t mV = esp_adc_cal_raw_to_voltage(adc1_get_raw(adc_channel), &adc1_chars);
-    return mV;
+static double analog_read(adc1_channel_t adc_channel){
+    double volts = esp_adc_cal_raw_to_voltage(adc1_get_raw(adc_channel), &adc1_chars) / 1000.0;
+    return volts;
 }
 
 //Cooler
 #define Cooler_Temp_ADC     (adc1_channel_t)ADC1_CHANNEL_0              //Analog In - ADC1->Channel 0 - GPIO1 
-int raw_cooler_Temp;
+double raw_cooler_Temp;
 double cooler_Temp;
+int resistance;
 static void read_cooler_temp(void){
     raw_cooler_Temp = analog_read(Cooler_Temp_ADC);
-    int resistance = (10 * raw_cooler_Temp + 16500) / (4.95 - raw_cooler_Temp);
-    cooler_Temp = 0.0000001 * (resistance^2) - 0.005*resistance + 67.3;
+    resistance = (10000 * raw_cooler_Temp + 16500) / (4.950 - raw_cooler_Temp);
+    cooler_Temp = (0.0000001*resistance*resistance) - (0.005*resistance) + 67.3;
 }
 
 #define Cooler_Voltage_ADC  (adc1_channel_t)ADC1_CHANNEL_1              //Analog In - ADC1->Channel 1 - GPIO2
-int raw_cooler_Voltage;
 double cooler_Voltage;
 static void read_cooler_voltage(void){
-    raw_cooler_Voltage = analog_read(Cooler_Voltage_ADC);
-    cooler_Voltage = raw_cooler_Voltage / 272.0;    // 272 mV/V
+    double raw_cooler_Voltage = analog_read(Cooler_Voltage_ADC);
+    cooler_Voltage = raw_cooler_Voltage / .272;    // 272 mV/V
 }
 
 #define Cooler_Current_ADC  (adc1_channel_t)ADC1_CHANNEL_2              //Analog In - ADC1->Channel 2 - GPIO3
-int raw_cooler_Current;
 double cooler_Current;
 static void read_cooler_current(void){
-    raw_cooler_Current = analog_read(Cooler_Current_ADC);
-    cooler_Current = 0.866 * (raw_cooler_Current / 500.0) + 0.0134;    // 500 mV/A
+    double raw_cooler_Current = analog_read(Cooler_Current_ADC);
+    cooler_Current = 0.866 * (raw_cooler_Current / .250) + 0.0134;    // 250 mV/A
 }
 
 #define Cooler_PWM          (gpio_num_t)CONFIG_COOLER_PWM               //Analog Out - 
@@ -53,13 +61,11 @@ int duty_cycle = 50;
 #define Cooler_ENABLE       (gpio_num_t)CONFIG_COOLER_ENABLE            //Digital Out
 #define Fan_Control         (gpio_num_t)CONFIG_FAN_CONTROL              //Digital Out
 bool cooler_enabled = false;
-static void enable_cooler(void)
-{
+static void enable_cooler(void){
     gpio_set_level(Cooler_ENABLE, 1);
     gpio_set_level(Fan_Control, 1);
 }
-static void disable_cooler(void)
-{
+static void disable_cooler(void){
     gpio_set_level(Cooler_ENABLE, 0);
     gpio_set_level(Fan_Control, 0);
 }
@@ -73,28 +79,27 @@ static void configure_cooler(void){
     gpio_set_direction(Cooler_ENABLE, GPIO_MODE_OUTPUT);
     gpio_set_direction(Fan_Control, GPIO_MODE_OUTPUT);
 
-    //Configure PWM - Timer
+    // Prepare and then apply the COOLER PWM timer configuration
     ledc_timer_config_t ledc_timer = {
-        .duty_resolution = LEDC_TIMER_10_BIT,
-        .freq_hz = 1000,
-        .speed_mode = LEDC_SPEED_MODE_MAX,
-        .timer_num = LEDC_TIMER_0,
-        .clk_cfg = LEDC_AUTO_CLK,
+        .speed_mode       = COOLER_MODE,
+        .duty_resolution  = COOLER_DUTY_RES,
+        .timer_num        = COOLER_TIMER,
+        .freq_hz          = COOLER_FREQUENCY,  // Set output frequency at 4 kHz
+        .clk_cfg          = LEDC_AUTO_CLK
     };
+    ESP_ERROR_CHECK(ledc_timer_config(&ledc_timer));
 
-    ledc_timer_config(&ledc_timer);
-    cooler_pwm_channel.channel = LEDC_CHANNEL_0;
-    cooler_pwm_channel.duty = 0;
-    cooler_pwm_channel.gpio_num = Cooler_PWM;
-    cooler_pwm_channel.speed_mode = LEDC_SPEED_MODE_MAX;
-    cooler_pwm_channel.hpoint = 0;
-    cooler_pwm_channel.timer_sel = LEDC_TIMER_0;
-    ledc_channel_config(&cooler_pwm_channel);
-
-    ledc_set_duty(cooler_pwm_channel.speed_mode, cooler_pwm_channel.channel, duty_cycle);
-    ledc_update_duty(cooler_pwm_channel.speed_mode, cooler_pwm_channel.channel);
-
-
+    // Prepare and then apply the COOLER PWM channel configuration
+    ledc_channel_config_t ledc_channel = {
+        .speed_mode     = COOLER_MODE,
+        .channel        = COOLER_CHANNEL,
+        .timer_sel      = COOLER_TIMER,
+        .intr_type      = LEDC_INTR_DISABLE,
+        .gpio_num       = COOLER_OUTPUT_IO,
+        .duty           = 0, // Set duty to 0%
+        .hpoint         = 0
+    };
+    ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
     disable_cooler();
 }
 
@@ -116,14 +121,12 @@ static void configure_leds(void){
     gpio_set_direction(PCB_Debug_LED, GPIO_MODE_OUTPUT);
 }
 
-static void enable_LED(gpio_num_t led_gpio)
-{
+static void enable_LED(gpio_num_t led_gpio){
     if(GPIO_IS_VALID_GPIO(led_gpio)){
         gpio_set_level(led_gpio, 1);
     }
 }
-static void disable_LED(gpio_num_t led_gpio)
-{
+static void disable_LED(gpio_num_t led_gpio){
     if(GPIO_IS_VALID_GPIO(led_gpio)){
         gpio_set_level(led_gpio, 0);
     }
@@ -133,12 +136,12 @@ static void disable_LED(gpio_num_t led_gpio)
 #define Lid_Status          (gpio_num_t)CONFIG_LID_HALL_SENSOR          //Digital In
 bool lid_closed = false;
 static void check_lid_closed(void){
-    lid_closed = gpio_get_level(Lid_Status);
+    lid_closed = !(gpio_get_level(Lid_Status));
 }
-#define Filled_Status       (gpio_num_t)CONFIG_COOLER_PRESSURE_SENSOR   //Analog In
+#define Filled_Status       (gpio_num_t)CONFIG_COOLER_PRESSURE_SENSOR   //Digital In
 bool container_filled = false;
 static void check_container_filled(void){
-    container_filled = gpio_get_level(Filled_Status);
+    container_filled = !(gpio_get_level(Filled_Status));
 }
 
 static void configure_sensors(void){
@@ -152,13 +155,11 @@ static void configure_sensors(void){
 //Lock
 #define Lock_Control        (gpio_num_t)CONFIG_LOCK_CONTROL             //Digital Out
 bool container_locked = false;
-static void lock_container(void)
-{
+static void lock_container(void){
     gpio_set_level(Lock_Control, 1);
     container_locked = true;
 }
-static void unlock_container(void)
-{
+static void unlock_container(void){
     gpio_set_level(Lock_Control, 0);
     container_locked = false;
 }
@@ -199,14 +200,13 @@ static void configure_keypad(void){
 #define ADC_SDA             (gpio_num_t)CONFIG_ADC_SDA
 #define ADC_SCL             (gpio_num_t)CONFIG_ADC_SCL
 
-static uint8_t s_led_state = 0;
+static uint8_t cooler_on = 0;
 
 //Peripheral Configurations
-static void configure_peripherals(void)
-{
+static void configure_peripherals(void){
     //ADC Configuration
-    adc1_config_channel_atten(ADC1_CHANNEL_0, ADC_ATTEN_DB_12);
-    esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_12, ADC_WIDTH_BIT_DEFAULT, 0, &adc1_chars);
+    adc1_config_channel_atten(ADC1_CHANNEL_0, ADC_ATTEN_DB_11);
+    esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_DEFAULT, 1100, &adc1_chars);
     adc1_config_width(ADC_WIDTH_BIT_DEFAULT);
 
     //Peripherals
@@ -218,17 +218,35 @@ static void configure_peripherals(void)
     ESP_LOGI(TAG, "Peripherals configured to capstone configuration.");
 }
 
+static void read_display_status(void){
+    read_cooler_temp();
+    read_cooler_voltage();
+    read_cooler_current();
+    check_container_filled();
+    check_lid_closed();
+    ESP_LOGI(TAG, "Raw Temp: %f\nResistance: %d\nTemp: %f C\n", raw_cooler_Temp, resistance, cooler_Temp);
+    ESP_LOGI(TAG, "Current: %f A\nVoltage: %f\n", cooler_Current, cooler_Voltage);
+    ESP_LOGI(TAG, "Container: %s\n Lid: %s\n", (container_filled == true) ? "Filled" : "Empty", (lid_closed == true) ? "Closed" : "Open");
+}
+
 void app_main(void)
 {
     /* Configure all peripherals */
     configure_peripherals();
+    // Set duty to 50%
+    ESP_ERROR_CHECK(ledc_set_duty(COOLER_MODE, COOLER_CHANNEL, COOLER_DUTY));
+    // Update duty to apply the new value
+    ESP_ERROR_CHECK(ledc_update_duty(COOLER_MODE, COOLER_CHANNEL));
+
+    cooler_on = true;
+    ESP_LOGI(TAG, "Turning cooler on!");
+    enable_cooler();
 
     while (1) {
-        ESP_LOGI(TAG, "Turning the LED %s!", s_led_state == true ? "ON" : "OFF");
-        /* Toggle the LED state */
-        s_led_state = !s_led_state;
-
-
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        read_display_status();
+        lock_container();
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
+        unlock_container();
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
     }
 }
